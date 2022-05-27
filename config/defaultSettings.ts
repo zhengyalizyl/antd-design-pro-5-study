1dfa2fd836eb5d26bf39aef8158ba70e16e6ecab
lizheng <v-lizheng@expediagroup.com>
2022-05-27 17:52:32 +0800

    first commit

---
+++ config/defaultSettings.ts
@@ -0,0 +1,21 @@
+import { Settings as LayoutSettings } from '@ant-design/pro-components';
+
+const Settings: LayoutSettings & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: 'Ant Design Pro',
+  pwa: false,
+  // logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',//这里存在争议
+  iconfontUrl: '',
+};
+
+export default Settings;
